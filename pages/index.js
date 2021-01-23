9f31c9b1a02c0ad9517d428313284b44571b052d
JeremyEllingham <jeremy@sethindustries.com.au>
2021-01-23 17:37:53 +0000

    Export recommended links.

--- pages/index.js
+++ pages/index.js
@@ -3,6 +3,7 @@ import styles from '../styles/Home.module.css'
 import Grid from '@material-ui/core/Grid'
 import ReactPlayer from 'react-player/youtube'
 import RecommendedLink from '../components/RecommendedLink'
+import recommendedLinks from './recommendedLinks'
 
 const myStyles = {
   container: {
@@ -30,44 +31,6 @@ const myStyles = {
     paddingLeft: 10
   }
 }
-
-const recommendedLinks = [
-  {
-    url: "https://www.bitcoin.com/bitcoin.pdf",
-    title: 'The Bitcoin Whitepaper',
-    description: 'By Satoshi Nakamoto. Read it.'
-  }, 
-  {
-    url: "https://www.youtube.com/watch?v=-jUsSEOqZME&feature=emb_title",
-    title: '10 Years of Bitcoin',
-    description: 'Hayden Otto explains in a 1 hour talk how it all came to be, Bitcoin Cash vs Bitcoin etc.'
-  },
-  {
-    url: 'https://txstreet.com/v/bch-btc',
-    title: 'TxStreet',
-    description: "Watch Bitcoin Cash's smooth bus station overtake Bitcoin's overcrowded mess.",
-  },
-  {
-    url: 'https://cash.coin.dance/blocks/transactions',
-    title: 'Coin.dance',
-    description: 'Keep up on the latest Bitcoin Cash stats.',
-  },
-  {
-    url: 'https://reddit.com/r/btc',
-    title: 'Reddit /r/btc',
-    description: ' Discuss or keep up with the Bitcoin Cash community.',
-  },
-  {
-    url: 'https://nakamotoinstitute.org/mempool/',
-    title: 'Nakamoto Institute Mempool',
-    description: 'Read up on some big picture Bitcoin theory.',
-  },
-  {
-    url: 'https://99bitcoins.com/bitcoin-obituaries/',
-    title: 'Bitcoin Obituaries',
-    description: 'Declared dead 350+ times for over 10 years. RIP the haters.',
-  }
-]
 
 const Home = () => {
   return (
---
+++ pages/recommendedLinks.js
@@ -0,0 +1,39 @@
+const recommendedLinks = [
+    {
+      url: "https://www.bitcoin.com/bitcoin.pdf",
+      title: 'The Bitcoin Whitepaper',
+      description: 'By Satoshi Nakamoto. Read it.'
+    }, 
+    {
+      url: "https://www.youtube.com/watch?v=-jUsSEOqZME&feature=emb_title",
+      title: '10 Years of Bitcoin',
+      description: 'Hayden Otto explains in a 1 hour talk how it all came to be, Bitcoin Cash vs Bitcoin etc.'
+    },
+    {
+      url: 'https://txstreet.com/v/bch-btc',
+      title: 'TxStreet',
+      description: "Watch Bitcoin Cash's smooth bus station overtake Bitcoin's overcrowded mess.",
+    },
+    {
+      url: 'https://cash.coin.dance/blocks/transactions',
+      title: 'Coin.dance',
+      description: 'Keep up on the latest Bitcoin Cash stats.',
+    },
+    {
+      url: 'https://reddit.com/r/btc',
+      title: 'Reddit /r/btc',
+      description: ' Discuss or keep up with the Bitcoin Cash community.',
+    },
+    {
+      url: 'https://nakamotoinstitute.org/mempool/',
+      title: 'Nakamoto Institute Mempool',
+      description: 'Read up on some big picture Bitcoin theory.',
+    },
+    {
+      url: 'https://99bitcoins.com/bitcoin-obituaries/',
+      title: 'Bitcoin Obituaries',
+      description: 'Declared dead 350+ times for over 10 years. RIP the haters.',
+    }
+  ]
+
+  export default recommendedLinks
