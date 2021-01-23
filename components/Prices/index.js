8ff79f58aa70b692b5343d845d4e42b389be079e
JeremyEllingham <jeremy@sethindustries.com.au>
2021-01-23 18:28:32 +0000

    Adding prices.

--- components/Prices/index.js
+++ components/Prices/index.js
@@ -1,20 +1,40 @@
-import React, { useEffect } from 'react'
+import React, { useState, useEffect } from 'react'
 import CoinGecko from 'coingecko-api';
 
 const CoinGeckoClient = new CoinGecko();
 
 const Prices = () => {
+  const [mBchPerUsd, setMBchPerUsd] = useState(0)
+  const [mBchPerGbp, setMBchPerGbp] = useState(0)
+  
   useEffect(() => {
     const grabData = async () => {
-      const data = await CoinGeckoClient.ping();
-      console.log('data: ', data)
+      const data = await CoinGeckoClient.coins.fetch('bitcoin-cash', {});
+
+      const usdPrice = data?.data?.market_data?.current_price?.usd
+      const bchPerUsd = 1 / usdPrice
+      const calcMBchPerUsd = Number(bchPerUsd * 1000).toFixed(3)
+      setMBchPerUsd(calcMBchPerUsd)
+
+      const gbpPrice = data?.data?.market_data?.current_price?.gbp
+      const bchPerGbp = 1 / gbpPrice
+      const calcMBchPerGbp = Number(bchPerGbp * 1000).toFixed(3)
+      setMBchPerGbp(calcMBchPerGbp)
     }
 
     grabData()
   }, [])
 
   return (
-    <h2>Prices</h2>
+    <div>
+      <h2>Prices</h2>
+      <p>1 mBCH (milli Bitcoin Cash) = 0.001 BCH</p>
+      <p>1 USD buys {mBchPerUsd}mBCH</p>
+      <p>1 GBP buys {mBchPerGbp}mBCH</p>
+      <p>1 EUR buys {mBchPerUsd}mBCH</p>
+      <p>1 CNY buys {mBchPerUsd}mBCH</p>
+      <p>1 AUD buys {mBchPerUsd}mBCH</p>
+    </div>
   )
 }
 
