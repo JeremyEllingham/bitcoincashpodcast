ef167f7fcedddc66d43f3da4de30884c637189c7
JeremyEllingham <jeremy@sethindustries.com.au>
2021-01-28 17:42:54 +0000

    Add direct download.

--- components/EpisodesList/index.js
+++ components/EpisodesList/index.js
@@ -1,12 +1,29 @@
 import React from 'react'
 import ReactPlayer from 'react-player/youtube'
 
+const styles = {
+  container: { 
+    display: 'flex', 
+    flexDirection: 'column', 
+    justifyContent: 'center', 
+    alignItems: 'center' 
+  },
+  player: {
+    paddingTop: 10
+  }
+}
+
 const EpisodesList = () => (
-  <div style={{ display: 'flex', flexDirection: 'column', justifyContent: 'center', alignItems: 'center' }}>
+  <div style={styles.container}>
     <div>
       <h3>#1: Whitepaper Whitewashing & History of The Flippening</h3>
       <p>Friday, 22nd January 2021 - BCH: 13u4JV4Ace94evGuujUFfh5VgNLV3jsTHp</p>
-      <ReactPlayer url='https://www.youtube.com/watch?v=_uimCmw1m9E' />
+      <a href="/mp3/TheBitcoinCashPodcastEp1.mp3"><u>Episode #1: Direct link to downloadable .mp3 file</u></a>
+      <ReactPlayer 
+        style={styles.player} 
+        url='https://www.youtube.com/watch?v=_uimCmw1m9E' 
+        target="_blank"
+      />
     </div>
   </div>
 )
