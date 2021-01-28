39b019018f28b2b77358c8d12cf04e3cb8fffdbf
JeremyEllingham <jeremy@sethindustries.com.au>
2021-01-28 18:00:03 +0000

    Setting up number 2.

--- components/EpisodesList/index.js
+++ components/EpisodesList/index.js
@@ -14,8 +14,19 @@ const styles = {
 }
 
 const EpisodesList = () => (
-  <div style={styles.container}>
-    <div>
+  <div>
+    <div style={styles.container}>
+      <h3>#2: Grassroots adoption & Wall Street vs /r/wallstreetbets</h3>
+      <p>Thursday, 28th January 2021 - BCH: bitcoincash:qqldcn9p2qupgkmq4ctrqujyd4ju2m94y5fq680hye</p>
+      <a href="/mp3/TheBitcoinCashPodcastEp2.mp3"><u>Episode #2: Direct link to downloadable .mp3 file</u></a>
+      <ReactPlayer 
+        style={styles.player} 
+        url='https://www.youtube.com/watch?v=_uimCmw1m9E' 
+        target="_blank"
+      />
+    </div>
+    <hr />
+    <div style={styles.container}>
       <h3>#1: Whitepaper Whitewashing & History of The Flippening</h3>
       <p>Friday, 22nd January 2021 - BCH: 13u4JV4Ace94evGuujUFfh5VgNLV3jsTHp</p>
       <a href="/mp3/TheBitcoinCashPodcastEp1.mp3"><u>Episode #1: Direct link to downloadable .mp3 file</u></a>
