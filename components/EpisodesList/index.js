9ec2ac28c08bc9087d31d9f96f52607efe8365f9
JeremyEllingham <jeremy@sethindustries.com.au>
2021-01-30 22:39:05 +0000

    Add link.

--- components/EpisodesList/index.js
+++ components/EpisodesList/index.js
@@ -35,7 +35,7 @@ const episodes = [
     id: 3,
     title: 'Elon Musk hypes Bitcoin & Crypto Convert Q&A',
     date: 'Saturday, 30th January 2021',
-    bchAddress: 'bitcoincash:qqldcn9p2qupgkmq4ctrqujyd4ju2m94y5fq680hye',
+    bchAddress: 'bitcoincash:qzm49092tapkkuzvlr2q5hkv0un6rucq4vvspptszt',
     mp3: "https://drive.google.com/file/d/1ml9J4zMKoU8ogvGvgN5l2cDq3eO0jTgo/view?usp=sharing",
     slideDeck: "https://docs.google.com/presentation/d/1BpjPtjRl15VQ2ETqFG20ACtNu0lar3aXs5diLCsdIGc/edit?usp=sharing",
     youtube: 'https://youtu.be/6ZB9Oa2TUjk',
