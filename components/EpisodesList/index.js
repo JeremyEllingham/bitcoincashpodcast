059d18415b30e4923dcc38b003d4582988b77a01
JeremyEllingham <jeremy@sethindustries.com.au>
2021-01-30 22:10:55 +0000

    Refactor into episodes list.

--- components/EpisodesList/index.js
+++ components/EpisodesList/index.js
@@ -13,29 +13,46 @@ const styles = {
   }
 }
 
+const episodes = [
+  {
+    id: 2,
+    title: 'Grassroots adoption & /r/wallstreetbets vs Wall Street',
+    date: 'Thursday, 28th January 2021',
+    bchAddress: 'bitcoincash:qqldcn9p2qupgkmq4ctrqujyd4ju2m94y5fq680hye',
+    mp3: "https://drive.google.com/file/d/1H4Hx3hGGA7iDQj5YluuC9sKGGNPUYmTM/view?usp=sharing",
+    slideDeck: "https://docs.google.com/presentation/d/1wshYxnpvvBe_e4PnzNIEQwVCSCi8AAzldGyuHQNfLB8/edit?usp=sharing",
+    youtube: 'https://www.youtube.com/watch?v=voVS2omxMek',
+  }
+]
+
 const EpisodesList = () => (
   <div>
-    <div style={styles.container}>
-      <h3>#2: Grassroots adoption & /r/wallstreetbets vs Wall Street</h3>
-      <p>Thursday, 28th January 2021 - bitcoincash:qqldcn9p2qupgkmq4ctrqujyd4ju2m94y5fq680hye</p>
-      <a 
-        href="https://drive.google.com/file/d/1H4Hx3hGGA7iDQj5YluuC9sKGGNPUYmTM/view?usp=sharing"
-        target="_blank"
-      ><u>Downloadable .mp3</u>
-      </a>
-      <a 
-        href="https://docs.google.com/presentation/d/1wshYxnpvvBe_e4PnzNIEQwVCSCi8AAzldGyuHQNfLB8/edit?usp=sharing" 
-        target="_blank"
-      >
-        <u>Slide deck</u>
-      </a>
-      <ReactPlayer 
-        style={styles.player} 
-        url='https://www.youtube.com/watch?v=voVS2omxMek' 
-        target="_blank"
-      />
-    </div>
-    <hr />
+    {episodes?.map(({ id, title, date, bchAddress, mp3, slideDeck, youtube}) => (
+      <div>
+        <div style={styles.container}>
+          <h3>#{id}: {title}</h3>
+          <p>{date} - {bchAddress}</p>
+          <a 
+            href={mp3}
+            target="_blank"
+          ><u>Downloadable .mp3</u>
+          </a>
+          <a 
+            href={slideDeck}
+            target="_blank"
+          >
+            <u>Slide deck</u>
+          </a>
+          <ReactPlayer 
+            style={styles.player} 
+            url={youtube}
+            target="_blank"
+          />
+        </div>
+        <hr />
+      </div>
+    ))}
+    
     <div style={styles.container}>
       <h3>#1: Whitepaper Whitewashing & History of The Flippening</h3>
       <p>Friday, 22nd January 2021 - bitcoincash:qq0u3gej2rw9elpe42pp4cy25qknh2r0fvgfnpv7qm</p>
