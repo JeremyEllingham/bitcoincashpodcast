47330b24445a6294080592aa5bc43eb4736625c2
JeremyEllingham <jeremy@sethindustries.com.au>
2021-01-30 22:15:46 +0000

    Add episode 3.

--- components/EpisodesList/index.js
+++ components/EpisodesList/index.js
@@ -30,13 +30,22 @@ const episodes = [
     mp3: "https://drive.google.com/file/d/1H4Hx3hGGA7iDQj5YluuC9sKGGNPUYmTM/view?usp=sharing",
     slideDeck: "https://docs.google.com/presentation/d/1wshYxnpvvBe_e4PnzNIEQwVCSCi8AAzldGyuHQNfLB8/edit?usp=sharing",
     youtube: 'https://www.youtube.com/watch?v=voVS2omxMek',
+  },
+  {
+    id: 3,
+    title: 'Elon Musk hypes Bitcoin & Crypto Convert Q&A',
+    date: 'Saturday, 30th January 2021',
+    bchAddress: 'bitcoincash:qqldcn9p2qupgkmq4ctrqujyd4ju2m94y5fq680hye',
+    mp3: "",
+    slideDeck: "https://docs.google.com/presentation/d/1BpjPtjRl15VQ2ETqFG20ACtNu0lar3aXs5diLCsdIGc/edit?usp=sharing",
+    youtube: '',
   }
 ]
 
 const EpisodesList = () => (
   <div>
-    {episodes?.map(({ id, title, date, bchAddress, mp3, slideDeck, youtube}) => (
-      <div>
+    {episodes?.reverse()?.map(({ id, title, date, bchAddress, mp3, slideDeck, youtube}) => (
+      <div key={id}>
         <div style={styles.container}>
           <h3>#{id}: {title}</h3>
           <p>{date} - {bchAddress}</p>
