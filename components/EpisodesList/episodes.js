813d525633aacad37f199535039dc0594438a1e7
JeremyEllingham <jeremy@sethindustries.com.au>
2021-02-06 21:18:23 +0000

    Add dates.

--- components/EpisodesList/episodes.js
+++ components/EpisodesList/episodes.js
@@ -1,16 +1,20 @@
+import moment from 'moment'
+
 const episodes = [
   {
     id: 1,
     title: 'Whitepaper Whitewashing & History of The Flippening',
     date: 'Friday, 22nd January 2021',
+    dateRSS: new Date(2021, 0, 22),
     bchAddress: 'bitcoincash:qq0u3gej2rw9elpe42pp4cy25qknh2r0fvgfnpv7qm',
     mp3: "https://drive.google.com/file/d/1b_Ra-IX1Lxt6X4doRVeLnxOogsaLwl1a/view?usp=sharing",
-    youtube: 'https://www.youtube.com/watch?v=_uimCmw1m9E' 
+    youtube: 'https://www.youtube.com/watch?v=_uimCmw1m9E',
   },
   {
     id: 2,
     title: 'Grassroots adoption & /r/wallstreetbets vs Wall Street',
     date: 'Thursday, 28th January 2021',
+    dateRSS: new Date(2021, 0, 28),
     bchAddress: 'bitcoincash:qqldcn9p2qupgkmq4ctrqujyd4ju2m94y5fq680hye',
     mp3: "https://drive.google.com/file/d/1H4Hx3hGGA7iDQj5YluuC9sKGGNPUYmTM/view?usp=sharing",
     slideDeck: "https://docs.google.com/presentation/d/1wshYxnpvvBe_e4PnzNIEQwVCSCi8AAzldGyuHQNfLB8/edit?usp=sharing",
@@ -20,6 +24,7 @@ const episodes = [
     id: 3,
     title: 'Elon Musk hypes Bitcoin & Crypto Convert Q&A',
     date: 'Saturday, 30th January 2021',
+    dateRSS: new Date(2021, 0, 30),
     bchAddress: 'bitcoincash:qzm49092tapkkuzvlr2q5hkv0un6rucq4vvspptszt',
     mp3: "https://drive.google.com/file/d/1ml9J4zMKoU8ogvGvgN5l2cDq3eO0jTgo/view?usp=sharing",
     slideDeck: "https://docs.google.com/presentation/d/1BpjPtjRl15VQ2ETqFG20ACtNu0lar3aXs5diLCsdIGc/edit?usp=sharing",
