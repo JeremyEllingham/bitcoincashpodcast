66db01a28985e1e3122bc2e515b756d8b61a3092
JeremyEllingham <jeremy@sethindustries.com.au>
2021-03-03 00:39:51 +0000

    Recommended links.

--- components/RecommendedLinks/links.js
+++ components/RecommendedLinks/links.js
@@ -90,6 +90,11 @@ const links = [
       description: 'Declared dead 350+ times for over 10 years. RIP the haters.',
     },
     {
+      url: 'https://bitcoincash.org/',
+      title: 'BitcoinCash.org',
+      description: 'Informational site.',
+    },
+    {
       url: 'https://bitcoincash.org/roadmap/',
       title: 'BCH Roadmap',
       description: 'Follow the path to global adoption technically speaking.',
