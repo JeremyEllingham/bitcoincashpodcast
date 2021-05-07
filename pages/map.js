82492086ad09175acdb8d0d3c4f926ac675761c3
JeremyEllingham <jeremy@sethindustries.com.au>
2021-05-08 00:18:31 +0100

    Update map.

--- pages/map.js
+++ pages/map.js
@@ -21,22 +21,58 @@ const colours = {
 const data = {
   nodes: [
     // Level 1 Node
-    { id: "Bitcoin Cash", size: nodeSizes.level1, color: colours.red },
+    {
+      id: "Bitcoin Cash",
+      size: nodeSizes.level1,
+      color: colours.red,
+      x: 400,
+      y: 200,
+    },
     // Level 2 Node
-    { id: "Engineering", size: nodeSizes.level2, color: colours.blue },
-    { id: "Community Discussion", size: nodeSizes.level2, color: colours.blue },
+    {
+      id: "Engineering",
+      size: nodeSizes.level2,
+      color: colours.blue,
+      x: 400,
+      y: 250,
+    },
+    {
+      id: "Community Discussion",
+      size: nodeSizes.level2,
+      color: colours.blue,
+      x: 400,
+      y: 150,
+    },
+    // Community Discussion
+    {
+      id: "Reddit /r/btc",
+      size: nodeSizes.level3,
+      color: colours.green,
+      x: 350,
+      y: 100,
+    },
+    {
+      id: "Reddit /r/BitcoinCash",
+      size: nodeSizes.level3,
+      color: colours.green,
+      x: 450,
+      y: 100,
+    },
   ],
   links: [
     // Level 1 to Level 2
     { source: "Bitcoin Cash", target: "Engineering" },
     { source: "Bitcoin Cash", target: "Community Discussion" },
     // Community Discussion
+    { source: "Community Discussion", target: "Reddit /r/btc" },
+    { source: "Community Discussion", target: "Reddit /r/BitcoinCash" },
   ],
 };
 
 // the graph configuration, just override the ones you need
 const myConfig = {
   nodeHighlightBehavior: true,
+  staticGraph: true,
   node: {
     color: "lightgreen",
     size: 120,
@@ -59,6 +95,11 @@ const Map = () => {
   return (
     <div>
       <h1>Bitcoin Cash Community Map</h1>
+      <h2>Notice something missing? Want to contribute?</h2>
+      <p>
+        Pull requests welcome:
+        https://github.com/JeremyEllingham/bitcoincashpodcast
+      </p>
       <Graph
         id="graph-id" // id is mandatory
         data={data}
