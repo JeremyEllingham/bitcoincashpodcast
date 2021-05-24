52b4d9ea8645428556fb3d380dc9d029009a2cf2
JeremyEllingham <jeremy@sethindustries.com.au>
2021-05-24 17:19:05 +0100

    Add link.

--- components/RecommendedLinks/links.js
+++ components/RecommendedLinks/links.js
@@ -1,139 +1,151 @@
 const links = [
-    {
-      url: "https://www.bitcoin.com/bitcoin.pdf",
-      title: 'The Bitcoin Whitepaper',
-      description: 'By Satoshi Nakamoto. Read it.'
-    },
-    {
-      url: 'https://www.youtube.com/watch?v=iFDe5kUUyT0',
-      title: 'The Biggest Scam In The History Of Mankind',
-      description: 'Mike Maloney explains the US dollar system.',
-    }, 
-    {
-      url: "https://www.youtube.com/watch?v=-jUsSEOqZME&feature=emb_title",
-      title: '10 Years of Bitcoin',
-      description: 'Hayden Otto explains in a 1 hour talk the origins of Bitcoin, Bitcoin Cash vs Bitcoin etc.'
-    },
-    {
-      url: 'https://txstreet.com/v/bch-btc',
-      title: 'TxStreet',
-      description: "Watch Bitcoin Cash's smooth bus station overtake Bitcoin's overcrowded mess.",
-    },
-    {
-      url: "https://lbry.tv/@freedomain:b/crypto_roundtable_10_history_of_revolution:9",
-      title: 'Bitcoin: The History Of A Revolution',
-      description: 'Stefan Molyneux and others discuss in detail the history of Bitcoin and the scaling debate.'
-    },
-    {
-      url: "https://satoshi.nakamotoinstitute.org/",
-      title: 'The Complete Satoshi',
-      description: "Cryptocurrency's progenitor's full ideas."
-    },
-    {
-      url: 'https://cash.coin.dance/blocks/transactions',
-      title: 'Coin.dance',
-      description: 'Keep up on the latest Bitcoin Cash stats.',
-    },
-    {
-      url: "https://whycryptocurrencies.com/",
-      title: 'Why Cryptocurrencies',
-      description: 'What they are, what they do and why they matter.'
-    },
-    {
-      url: "https://falkvinge.net/wp-content/uploads/2017/11/letter-from-the-ceo.pdf",
-      title: 'Letter From The CEO',
-      description: 'Rick Falkvinge outlines a strategy for the BCH community.'
-    },
-    {
-      url: 'https://reddit.com/r/btc',
-      title: 'Reddit /r/btc',
-      description: ' Discuss or keep up with the Bitcoin Cash community.',
-    },
-    {
-      url: "https://www.youtube.com/watch?v=mvcZNSwQlRU",
-      title: 'A Quality Game Of Poker',
-      description: 'Kim Dotcom talks to BTC Core supporters about Bitcoin.'
-    },
-    {
-      url: "http://blog.vermorel.com/journal/2017/12/17/terabyte-blocks-for-bitcoin-cash.html",
-      title: 'Terabyte blocks for Bitcoin Cash',
-      description: '2017 article explaining Bitcoin Cash as already feasible for 10 billion people.'
-    },
-    {
-      url: 'https://whybitcoincash.com/',
-      title: 'Why Bitcoin Cash',
-      description: "BCH vs BTC informational website.",
-    },
-    {
-      url: "https://read.cash/@CuriousTitmouse/history-of-rbitcoin-622951af",
-      title: 'History of /r/Bitcoin',
-      description: 'Detailed discussion of lead up to the BCH <> BTC fork.'
-    },
-    {
-      url: "https://www.youtube.com/watch?v=8KKiTd-3uNI",
-      title: 'Monetary Fundamentals with Jeffrey Tucker',
-      description: 'Jeffrey Tucker talks about cryptocurrency and sound money.'
-    },
-    {
-      url: "https://www.youtube.com/watch?v=zR4IOP4RBBs",
-      title: 'They Lie',
-      description: 'Hayden Otto discovers some special crypto sunglasses.'
-    },
-    {
-      url: 'https://www.youtube.com/watch?v=jctc85X_PCI&feature=emb_title',
-      title: 'We Are All Satoshi',
-      description: "Rick Falkvinge discusses Blocksteam's influence in BTC.",
-    },
-    {
-      url: "https://www.youtube.com/watch?v=2nBPN-MKefA",
-      title: 'Money as Debt',
-      description: 'Why everyone and every country is constantly drowning in debt.'
-    },
-    {
-      url: 'https://www.youtube.com/watch?v=CsCsxUM1y_w',
-      title: 'Blockworks',
-      description: "Charlie Shrem talks about the cryptocurrency space.",
-    },
-    {
-      url: 'https://nakamotoinstitute.org/mempool/',
-      title: 'Nakamoto Institute Mempool',
-      description: 'Read up on some big picture Bitcoin theory.',
-    },
-    {
-      url: 'https://99bitcoins.com/bitcoin-obituaries/',
-      title: 'Bitcoin Obituaries',
-      description: 'Declared dead 350+ times for over 10 years. RIP the haters.',
-    },
-    {
-      url: 'https://bitcoincash.org/',
-      title: 'BitcoinCash.org',
-      description: 'Informational site.',
-    },
-    {
-      url: 'https://cash.coin.dance/development',
-      title: 'BCH Development Upgrades',
-      description: 'Keep an eye on what tech is being added to BCH.',
-    },
-    {
-      url: 'https://www.youtube.com/watch?v=HggepmwdIBI',
-      title: 'BCH Network Discussions',
-      description: 'Keep an eye on what BCH development is happening.',
-    },
-    {
-      url: 'https://read.cash/@Mr-Zwets/the-year-2018-in-bitcoin-cash-a9d8e67c#the-technical-excuses',
-      title: 'The year 2018 in Bitcoin Cash',
-      description: 'BCH history, around the time of BCH <> BSV fork.',
-    },
-    {
-      url: 'https://bitcoincash.site/',
-      title: 'BitcoinCash.site',
-      description: 'Adoption platform, blog and information source in the developing world',
-    },
-    {
-      url: 'https://www.youtube.com/watch?v=r6-yBOC9oIs',
-      title: 'Roger Ver: A Short History of Bitcoin full of Intrigues, Censorship & The CIA?! ',
-      description: 'Roger Ver interviewed on the history of Bitcoin',
-    }
-  ]
+  {
+    url: "https://www.bitcoin.com/bitcoin.pdf",
+    title: "The Bitcoin Whitepaper",
+    description: "By Satoshi Nakamoto. Read it.",
+  },
+  {
+    url: "https://www.youtube.com/watch?v=iFDe5kUUyT0",
+    title: "The Biggest Scam In The History Of Mankind",
+    description: "Mike Maloney explains the US dollar system.",
+  },
+  {
+    url: "https://www.youtube.com/watch?v=-jUsSEOqZME&feature=emb_title",
+    title: "10 Years of Bitcoin",
+    description:
+      "Hayden Otto explains in a 1 hour talk the origins of Bitcoin, Bitcoin Cash vs Bitcoin etc.",
+  },
+  {
+    url: "https://txstreet.com/v/bch-btc",
+    title: "TxStreet",
+    description:
+      "Watch Bitcoin Cash's smooth bus station overtake Bitcoin's overcrowded mess.",
+  },
+  {
+    url: "https://lbry.tv/@freedomain:b/crypto_roundtable_10_history_of_revolution:9",
+    title: "Bitcoin: The History Of A Revolution",
+    description:
+      "Stefan Molyneux and others discuss in detail the history of Bitcoin and the scaling debate.",
+  },
+  {
+    url: "https://satoshi.nakamotoinstitute.org/",
+    title: "The Complete Satoshi",
+    description: "Cryptocurrency's progenitor's full ideas.",
+  },
+  {
+    url: "https://cash.coin.dance/blocks/transactions",
+    title: "Coin.dance",
+    description: "Keep up on the latest Bitcoin Cash stats.",
+  },
+  {
+    url: "https://whycryptocurrencies.com/",
+    title: "Why Cryptocurrencies",
+    description: "What they are, what they do and why they matter.",
+  },
+  {
+    url: "https://falkvinge.net/wp-content/uploads/2017/11/letter-from-the-ceo.pdf",
+    title: "Letter From The CEO",
+    description: "Rick Falkvinge outlines a strategy for the BCH community.",
+  },
+  {
+    url: "https://reddit.com/r/btc",
+    title: "Reddit /r/btc",
+    description: " Discuss or keep up with the Bitcoin Cash community.",
+  },
+  {
+    url: "https://www.youtube.com/watch?v=mvcZNSwQlRU",
+    title: "A Quality Game Of Poker",
+    description: "Kim Dotcom talks to BTC Core supporters about Bitcoin.",
+  },
+  {
+    url: "http://blog.vermorel.com/journal/2017/12/17/terabyte-blocks-for-bitcoin-cash.html",
+    title: "Terabyte blocks for Bitcoin Cash",
+    description:
+      "2017 article explaining Bitcoin Cash as already feasible for 10 billion people.",
+  },
+  {
+    url: "https://whybitcoincash.com/",
+    title: "Why Bitcoin Cash",
+    description: "BCH vs BTC informational website.",
+  },
+  {
+    url: "https://read.cash/@CuriousTitmouse/history-of-rbitcoin-622951af",
+    title: "History of /r/Bitcoin",
+    description: "Detailed discussion of lead up to the BCH <> BTC fork.",
+  },
+  {
+    url: "https://www.youtube.com/watch?v=8KKiTd-3uNI",
+    title: "Monetary Fundamentals with Jeffrey Tucker",
+    description: "Jeffrey Tucker talks about cryptocurrency and sound money.",
+  },
+  {
+    url: "https://www.youtube.com/watch?v=zR4IOP4RBBs",
+    title: "They Lie",
+    description: "Hayden Otto discovers some special crypto sunglasses.",
+  },
+  {
+    url: "https://www.youtube.com/watch?v=jctc85X_PCI&feature=emb_title",
+    title: "We Are All Satoshi",
+    description: "Rick Falkvinge discusses Blocksteam's influence in BTC.",
+  },
+  {
+    url: "https://www.youtube.com/watch?v=2nBPN-MKefA",
+    title: "Money as Debt",
+    description:
+      "Why everyone and every country is constantly drowning in debt.",
+  },
+  {
+    url: "https://www.youtube.com/watch?v=CsCsxUM1y_w",
+    title: "Blockworks",
+    description: "Charlie Shrem talks about the cryptocurrency space.",
+  },
+  {
+    url: "https://nakamotoinstitute.org/mempool/",
+    title: "Nakamoto Institute Mempool",
+    description: "Read up on some big picture Bitcoin theory.",
+  },
+  {
+    url: "https://99bitcoins.com/bitcoin-obituaries/",
+    title: "Bitcoin Obituaries",
+    description: "Declared dead 350+ times for over 10 years. RIP the haters.",
+  },
+  {
+    url: "https://bitcoincash.org/",
+    title: "BitcoinCash.org",
+    description: "Informational site.",
+  },
+  {
+    url: "https://cash.coin.dance/development",
+    title: "BCH Development Upgrades",
+    description: "Keep an eye on what tech is being added to BCH.",
+  },
+  {
+    url: "https://www.youtube.com/watch?v=HggepmwdIBI",
+    title: "BCH Network Discussions",
+    description: "Keep an eye on what BCH development is happening.",
+  },
+  {
+    url: "https://read.cash/@Mr-Zwets/the-year-2018-in-bitcoin-cash-a9d8e67c#the-technical-excuses",
+    title: "The year 2018 in Bitcoin Cash",
+    description: "BCH history, around the time of BCH <> BSV fork.",
+  },
+  {
+    url: "https://bitcoincash.site/",
+    title: "BitcoinCash.site",
+    description:
+      "Adoption platform, blog and information source in the developing world",
+  },
+  {
+    url: "https://www.youtube.com/watch?v=r6-yBOC9oIs",
+    title:
+      "Roger Ver: A Short History of Bitcoin full of Intrigues, Censorship & The CIA?! ",
+    description: "Roger Ver interviewed on the history of Bitcoin",
+  },
+  {
+    url: "https://read.cash/@Mr-Zwets/bsv-is-a-caricature-of-onchain-scaling-47b38dc9",
+    title: "BSV is a caricature of onchain scaling",
+    description: "BCH vs BTC vs BSV when it comes to scaling",
+  },
+];
 
-  export default links
+export default links;
