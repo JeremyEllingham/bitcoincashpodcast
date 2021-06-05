7d2453a70845ac05592423978fff6fe85008e296
JeremyEllingham <jeremy@sethindustries.com.au>
2021-06-05 17:57:09 +0100

    Add platform links.

--- components/PlatformsLinks/links.js
+++ components/PlatformsLinks/links.js
@@ -1,40 +1,50 @@
 const links = [
   {
     url: "https://www.youtube.com/channel/UCsrDsJnHFnkMnJhEslofyPQ",
-    title: 'Youtube',
-    description: 'Videos.'
+    title: "Youtube",
+    description: "Videos.",
+  },
+  {
+    url: "https://www.youtube.com/channel/UC22kOwMuIzJCOtAPjoIjkFA",
+    title: "Youtube (Clips channel)",
+    description: "Short clips for more digestible BCH Podcast.",
   },
   {
     url: "https://twitter.com/TheBCHPodcast",
-    title: 'Twitter',
-    description: 'I tweet when an episode is going live.'
+    title: "Twitter",
+    description: "I tweet when an episode is going live.",
   },
   {
     url: "https://read.cash/@BitcoinCashPodcast",
-    title: 'Read.cash',
-    description: "BCH blogging platform."
+    title: "Read.cash",
+    description: "BCH blogging platform.",
   },
   {
     url: "https://noise.cash/u/TheBitcoinCashPodcast",
-    title: 'Noise.cash',
-    description: "BCH Twitter."
+    title: "Noise.cash",
+    description: "BCH Twitter.",
+  },
+  {
+    url: "https://www.instagram.com/thebitcoincashpodcast/",
+    title: "Instagram",
+    description: "Photos.",
   },
   {
     url: "#",
-    title: 'Email',
-    description: "jeremy@bitcoincashpodcast.com"
+    title: "Email",
+    description: "jeremy@bitcoincashpodcast.com",
   },
   {
     url: "https://www.twitch.tv/thebitcoincashpodcast",
-    title: 'Twitch.tv',
-    description: 'Live streams.'
+    title: "Twitch.tv",
+    description: "Live streams.",
   },
   {
     url: "https://www.patreon.com/bitcoincashpodcast",
-    title: 'Patreon',
-    description: "Support the show on an ongoing basis. Pays in fiat (for now), but I use it all to buy BCH."
+    title: "Patreon",
+    description:
+      "Support the show on an ongoing basis. Pays in fiat (for now), but I use it all to buy BCH.",
   },
+];
 
-]
-
-export default links
+export default links;
