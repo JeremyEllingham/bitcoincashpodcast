140fface5543b471bbfa6db2012b5fecf38ade77
JeremyEllingham <jeremy@sethindustries.com.au>
2021-06-29 21:59:05 +0100

    Add colour comment.

--- components/DonationButton/index.js
+++ components/DonationButton/index.js
@@ -1,34 +1,37 @@
-import { PayButton } from '@paybutton/react'
+import { PayButton } from "@paybutton/react";
 
 const DonationButton = () => {
   const mySuccessFunction = () => {
-    console.log("Success!")
-  }
+    console.log("Success!");
+  };
 
-  const BCH_GREEN = '#84cb55'
-  const to = 'bitcoincash:qqq24rak0cea37qcnfuj2qvt5sdlpp8x9gnjunmqvn'
-  const amount = 5
-  const currency = 'USD'
-  const text = 'Donate'
-  const hoverText = 'Keeps the content rolling!'
+  // Dark GREY: #3a3a3a
+  const BCH_GREEN = "#84cb55";
+  const to = "bitcoincash:qqq24rak0cea37qcnfuj2qvt5sdlpp8x9gnjunmqvn";
+  const amount = 5;
+  const currency = "USD";
+  const text = "Donate";
+  const hoverText = "Keeps the content rolling!";
   const theme = {
     palette: {
       primary: BCH_GREEN,
-      secondary: '#E4E4E4',
-      tertiary: '#333333'
-    }
-  }
-  const onSuccess = mySuccessFunction()
+      secondary: "#E4E4E4",
+      tertiary: "#333333",
+    },
+  };
+  const onSuccess = mySuccessFunction();
 
-  return <PayButton
-    to={to}
-    amount={amount}
-    currency={currency}
-    text={text}
-    hoverText={hoverText}
-    theme={theme}
-    onSuccess={onSuccess}
-  />
-}
+  return (
+    <PayButton
+      to={to}
+      amount={amount}
+      currency={currency}
+      text={text}
+      hoverText={hoverText}
+      theme={theme}
+      onSuccess={onSuccess}
+    />
+  );
+};
 
-export default DonationButton
+export default DonationButton;
